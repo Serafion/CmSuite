b7853578494757a4d4ddfb75c7ca74791acdb6ab
Serafion <pzakrzewski87@gmail.com>
2023-04-01 20:59:33 +0200

    Initial commit

---
+++ src/main/java/com/contractmanagmentsuite/CmSuite/CmSuiteApplication.java
@@ -0,0 +1,13 @@
+package com.contractmanagmentsuite.CmSuite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmSuiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmSuiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/contractmanagmentsuite/CmSuite/CmSuiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.contractmanagmentsuite.CmSuite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmSuiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
